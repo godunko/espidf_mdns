57ab69910e7885a1978752e465dd8a2da4eb817a
Vadim Godunko <vgodunko@gmail.com>
2026-09-22 14:46:05 +0400

    Bind `mdns_hostname_set`

--- source/espidf-mdns.adb
+++ source/espidf-mdns.adb
@@ -17,4 +17,29 @@ package body ESPIDF.mDNS is
       Ada_ESP_Check_Error (mdns_init);
    end mdns_init;
 
+   -----------------------
+   -- mdns_hostname_set --
+   -----------------------
+
+   function mdns_hostname_set
+     (hostname : ESPIDF.C_Strings.char_array_string) return esp_err_t
+   is
+      function Imported
+        (hostname : ESPIDF.C_Strings.const_char_ptr) return esp_err_t
+        with Import, Convention => C, External_Name => "mdns_hostname_set";
+
+   begin
+      return Imported (ESPIDF.C_Strings.As_const_char_ptr (hostname));
+   end mdns_hostname_set;
+
+   -----------------------
+   -- mdns_hostname_set --
+   -----------------------
+
+   procedure mdns_hostname_set
+     (hostname : ESPIDF.C_Strings.char_array_string) is
+   begin
+      Ada_ESP_Check_Error (mdns_hostname_set (hostname));
+   end mdns_hostname_set;
+
 end ESPIDF.mDNS;
--- source/espidf-mdns.ads
+++ source/espidf-mdns.ads
@@ -4,11 +4,19 @@
 --  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 --
 
+with ESPIDF.C_Strings;
+
 package ESPIDF.mDNS is
 
    function mdns_init return esp_err_t
      with Import, Convention => C, External_Name => "mdns_init";
 
    procedure mdns_init;
+
+   function mdns_hostname_set
+     (hostname : ESPIDF.C_Strings.char_array_string) return esp_err_t;
+
+   procedure mdns_hostname_set
+     (hostname : ESPIDF.C_Strings.char_array_string);
 
 end ESPIDF.mDNS;
