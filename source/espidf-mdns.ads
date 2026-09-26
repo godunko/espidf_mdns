6528b14c877626ece853ef4be85bb9b3050b93a4
Vadim Godunko <vgodunko@gmail.com>
2026-09-26 21:28:17 +0400

    Add documentation

--- source/espidf-mdns.ads
+++ source/espidf-mdns.ads
@@ -10,13 +10,37 @@ package ESPIDF.mDNS is
 
    function mdns_init return esp_err_t
      with Import, Convention => C, External_Name => "mdns_init";
+   --  Initialize mDNS on given interface.
+   --  @return
+   --    - `ESP_OK` if mDNS was initialized successfully
+   --    - `ESP_ERR_INVALID_STATE` if event handler cannot be registered
+   --    - `ESP_ERR_NO_MEM` if memory cannot be allocated
+   --    - `ESP_FAIL` if mdns task cannot be started
 
    procedure mdns_init;
+   --  Initialize mDNS on given interface.
+   --  @raise ESPIDF_Error raised on error:
+   --    - `ESP_ERR_INVALID_STATE` if event handler cannot be registered
+   --    - `ESP_ERR_NO_MEM` if memory cannot be allocated
+   --    - `ESP_FAIL` if mdns task cannot be started
 
    function mdns_hostname_set
      (hostname : ESPIDF.C_Strings.char_array_string) return esp_err_t;
+   --  Set the hostname for mDNS server, required if you want to advertise
+   --  services.
+   --  @param hostname Hostname to set
+   --  @return
+   --    - `ESP_OK` if hostname was set successfully
+   --    - `ESP_ERR_INVALID_ARG` if parameters are invalid
+   --    - `ESP_ERR_NO_MEM` if memory cannot be allocated
 
    procedure mdns_hostname_set
      (hostname : ESPIDF.C_Strings.char_array_string);
+   --  Set the hostname for mDNS server, required if you want to advertise
+   --  services.
+   --  @param hostname Hostname to set
+   --  @raise ESPIDF_Error raised on error:
+   --    - `ESP_ERR_INVALID_ARG` if parameters are invalid
+   --    - `ESP_ERR_NO_MEM` if memory cannot be allocated
 
 end ESPIDF.mDNS;
